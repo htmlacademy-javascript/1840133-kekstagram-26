15be010119436bd163ecc2939dd29324c48b1624
Радик <a-radik-87@mail.ru>
2022-06-13 21:01:45 +0300

    Добавляет проверку в функцию рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const getRandomInteger = (min, max) => {
+  if (min > max) {
+    return('Неверно переданы аргументы');
+  }
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
